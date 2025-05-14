17d851fd52bf1191acce9a63e99fbf5521bd787f
victoruller <victoruller12@gmail.com.br>
2025-05-13 23:54:35 -0300

    semantico: agora tabela semântica é uma pilha de escopos que são HashMaps de simbolos. v1

--- src/sem/semanticTable.rs
+++ src/sem/semanticTable.rs
@@ -6,66 +6,82 @@ enum Tipo {
     bool,
     char,
     float,
-    int,
+    number,
+    string,
     unit,
 }
 
+struct SemanticTable {
+    scopeStack : Vec<Scope>
+}
+struct Scope {
+    symbles: HashMap<String, SemanticSymble> // O identifier fica como chave da map
+}
 struct SemanticSymbol {
-    tipo             : Tipo,
-    declarada        : bool,
-    inicializada     : bool,
-    escopo           : i32,
-    parametro        : bool,
-    posicaoParametro : i32,
-    vetor            : bool,
-    referencia       : bool,
-    lambda           : bool,
+  kind: SymbolKind,          // var, lambda, array, const
+}
+
+enum SymbolKind {
+    normal_value { // para variáveis, return_type de lambdas, return_type de arrays
+        return_type: Tipo,
+    },
+
+    Lambda {
+        return_type: Box<SymbolKind>, // Tipo de retorno da lambda (pode ser uma lambda)
+        param_types: Vec<SymbolKind>,
+    },
+
+    Array {
+        return_type: Box<SymbolKind>, // Tipo dos elementos do array
+        size: i32,
+    },
+    //Constant,
 }
 
 
 
 impl SemanticSymbol {
     fn new(
-        p_tipo : Tipo,
-        p_declarada : bool,
-        p_inicializada : bool,
-        p_escopo : int,
-        p_parametro : bool,
-        p_posicaoParametro : i32,
-        p_vetor : bool,
-        p_referencia : bool,
-        p_lambda : bool
+        kind: SymbolKind,
     ) -> SemanticSymbol {
         SemanticSymbol {
-            declarada: p_declarada,
-            inicializada: p_inicializada, 
-            escopo: p_escopo, 
-            parametro: p_parametro, 
-            posicaoParametro: p_posicaoParametro, 
-            vetor: p_vetor, 
-            referencia: p_referencia, 
-            lambda: p_lambda
+            kind: kind,
         }
         
     }
 }
 
-struct SemanticTable {
-    symbols : HashMap<String, SemanticSymbol>,
-    contId  : i32,
-}
 impl SemanticTable {
     fn new() -> SemanticTable{
         SemanticTable {symbols: HashMap<String, SemanticSymbol>::new(), contId: 0}
     }
 
-    fn add_symbol(&mut self, tipo: Tipo, declarada: bool, inicializada: bool, escopo: i32, parametro: bool, posicaoParametro: i32, vetor: bool, referencia: bool, lambda: bool) {
-        let symbol = SemanticSymbol::new(tipo, declarada, inicializada, escopo, parametro, posicaoParametro, vetor, referencia, lambda);
+    fn add_variable(&mut self, p_id: &str, p_type: Tipo) {
+        let symbol = SemanticSymbol::new(SymbolKind::normal_value {
+            return_type: p_type,
+        });
         self.symbols.insert(id.to_string(), symbol);
     }
 
     fn get_symbol(&self, id: &str) -> Option<&SemanticSymbol> {
-        self.symbols.get(id)
+        for scope in self.scopeStack.iter().rev() {
+            if let Some(symbol) = scope.symbols.get(id) {
+                return Some(symbol);
+            }
+        }
+        return None;
+    }
+
+
+
+
+    fn is_in_scope(&self, id: &str) -> bool {
+        for scope in self.scopeStack.iter().rev() {
+            if scope.symbols.contains_key(id) {
+                return true;
+            }
+        }
+        return false
     }
 
 }
